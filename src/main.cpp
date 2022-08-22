9d2e7f9c5bd39724c56ede854969e9a9f3a31dd8
PaulDieterich <Paul.dieterich0@gmail.com>
2022-08-22 11:29:40 +0200

    demo settings

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ int rawADC;
 String message;
 String response;
 int pumpStatus;
-int batteryStatus;
+uint8_t batteryStatus;
 Lorawan* lora = new Lorawan(); // my own LoraWAN class
 Soil* soil = new Soil(); // my own Soil class
 Pump *pump = new Pump(); // my own Pump class
@@ -33,19 +33,20 @@ void setup() {
   M5.begin();
   M5.Power.begin();
   M5.Lcd.begin();
-  M5.Lcd.wakeup();
+  //M5.Lcd.wakeup(); 
   lora->setUpLoRaWAN(response);
   pinMode(INPUT_PIN, INPUT);
   pinMode(PUMP_PIN, OUTPUT); 
   pinMode(MOISTURE_SENSOR_PIN, OUTPUT);
   digitalWrite(MOISTURE_SENSOR_PIN, 0);
+  //esp_sleep_enable_timer_wakeup(SLEEP_TIME);
 }
 
+
 void pumpIsNeeded(){
-  if(soil->getMoisture() > MOISTURE_MINIMUM){
+  if(soil->getMoisture() > MOISTURE_MEDIUM){
     pump->start();
-  }
-  if(soil->getMoisture() < MOISTURE_MAXIMUM){
+  }else if(soil->getMoisture() < MOISTURE_MINIMUM){
     //pump is on
       pump->stop();
   } 
@@ -56,7 +57,6 @@ void displayMoisture(){
   M5.Lcd.setCursor(0,0);
   M5.Lcd.print("Moisture: ");
   M5.Lcd.print(soil->getMoisture());
-  M5.Lcd.print("%");
 
 }
 
@@ -68,21 +68,24 @@ void loop() {
     loops++;
     rawADC = analogRead(INPUT_PIN);
     soil->setMoisture(rawADC);
-    displayMoisture();
+    if(DEBUG) displayMoisture();
     pumpIsNeeded();     
     Serial.printf("Massage String: %s\n", soil->getMoistureString().c_str());
     batteryStatus = M5.Power.getBatteryLevel();
     pumpStatus = pump->isPumpOn() ? 1 : 0;
     lora->send(soil->getMoisture() , pumpStatus, batteryStatus);
     response = lora->receive();
-
-    if(response = "ON"){
-      pump->start();
-    }
-  if(loops > 30){
-      M5.Power.deepSleep(SLEEP_TIME);
+    Serial.printf("loop: %d\n", loops);
+    if(loops > 30){
       loops = 0;
-   } 
-   M5.Lcd.clear();
+      pump->stop();
+      M5.Lcd.setBrightness(0); // turn off display  
+    //Syntax: esp_deep_sleep_start();
+    //void deepSleep(uint64_t time_in_us)
+    //This function shifts to deep sleep mode.
+    //It starts when the specified time or port status changes. After waking up, the CPU will be restarted instead of running from the next line.
+     M5.Power.deepSleep(SLEEP_SEC(SLEEP_TIME));
+     }
+     if(DEBUG) M5.Lcd.clear();
    }
 }
--- src/settings.h
+++ src/settings.h
@@ -14,16 +14,15 @@
 #define PUMP_PIN 26
 #define INPUT_PIN 36
 
-//time between measurements in ms
-#define SLEEP_TIME 10000
+//time between measurements in seconds
+#define SLEEP_TIME 60 //1 minutes
 
 //set Moisture Sensor Threshold
-#define MOISTURE_MINIMUM 1900//soil is dry af
-#define MOISTURE_MAXIMUM 1700 //soil is wet
+#define MOISTURE_MINIMUM 1900//soil is dry
+#define MOISTURE_MEDIUM  1700//soil is wet but not too wet
+#define MOISTURE_MAXIMUM 1600 //soil is wet
 //#define DURATION  15 //pump duration in seconds
 
 //debugging
 #define DEBUG 0
 
-//api call in c++
-//https://stackoverflow.com/questions/5707957/c-libcurl-json-rest/5780603#5780603
